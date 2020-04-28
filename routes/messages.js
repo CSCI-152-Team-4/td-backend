a17f3bc08964d86d4b93e751ffc63bf3bc2ae5ce
Andrew <andydrew313@gmail.com>
2020-04-27 21:41:13 -0700

    able to message, no sockets on em yet

--- routes/messages.js
+++ routes/messages.js
@@ -1,35 +1,53 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
-const Messages = require('../schemas/MessageSchema')
+const Users = require("../schemas/UserSchema");
+const Messages = require("../schemas/MessageSchema");
 /*
 router.get('/', (req, res, next) => {
     res.send(true)
   })
 */
-router.post('/', async (req, res) => {
-    const { body, sender, receiver } = req.body
-    try {
-      await Messages.create({
-        body: body,
-        sender: sender,
-        receiver: receiver,
-        dateCreated: new Date().getTime()
-      })
-      res.status(200).send(true)
-    } catch(err){
-      console.log('err', err)
-      res.status(500).send("err sending message")
-    }
-  })
+router.post("/", async (req, res) => {
+  const { body, sender, receiver } = req.body;
+  try {
+    await Messages.create({
+      body: body,
+      sender: sender,
+      receiver: receiver,
+    });
+    res.status(200).send(true);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send("err sending message");
+  }
+});
 
-router.get('/:limit', async (req, res) => {
-    try{
-      const messages = await Messages.find({}, "body sender receiver").sort("-createdAt").limit(Number(req.params.limit))
-      res.status(200).send(messages)
-    } catch(err){
-      console.log('err retrieving messages', err)
-      res.status(500).send(false)
-    }
-  })
+router.get("/:userId/:friendId", async (req, res) => {
+  const { userId, friendId } = req.params;
+  try {
+    const friendName = await Users.findById(friendId, "firstName");
+    const messages = await Messages.find({
+      $or: [
+        { sender: userId, receiver: friendId },
+        { sender: friendId, receiver: userId },
+      ],
+    }).sort("createdAt");
+    res.send({ messages, friendName });
+  } catch (err) {
+    console.log("err", err);
+    res.send([]);
+  }
+});
+
+router.get("/:limit", async (req, res) => {
+  try {
+    const messages = await Messages.find({}, "body sender receiver")
+      .sort("-createdAt")
+      .limit(Number(req.params.limit));
+    res.status(200).send(messages);
+  } catch (err) {
+    console.log("err retrieving messages", err);
+    res.status(500).send(false);
+  }
+});
 module.exports = router;
